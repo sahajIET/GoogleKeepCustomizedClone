46d129c079d423af0fe39575276546e51ec2a5d1
sahajIET <sahajbagherwal99@gmail.com>
2023-03-17 15:12:44 +0530

    Initial

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import EmojiObjectsSharpIcon from '@mui/icons-material/EmojiObjectsSharp';
+function Header() {
+  return (
+    <header>
+      <h1>
+      <EmojiObjectsSharpIcon />
+      Keep
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
